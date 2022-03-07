2fad4066bc70aa4d18945437d7dc5edd63d397db
Smita1706 <smitachinchkar1998@gmail.com>
2022-03-07 09:20:37 +0530

    Updated : Instead of single hotel adding multiple hotels

--- main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -4,34 +4,53 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HotelReservation {
-	int added = 0;
 	ArrayList<HotelInformation> hotels = new ArrayList<>();
-	Scanner scanner = new Scanner(System.in);
+	static Scanner s = new Scanner(System.in);
 	public boolean addHotel() {
 		HotelInformation hotel = new HotelInformation();
 		System.out.println("Enter hotel name: ");
-		hotel.setName(scanner.next());
+		hotel.setName(s.next());
 		System.out.println("Enter hotel rate: ");
-		hotel.setRating(scanner.nextInt());
+		hotel.setRating(s.nextInt());
 		hotels.add(hotel);
 		System.out.println("Hotel Added");
-		added++;
 		if(hotels.add(hotel))
 			return true;
 		else 
 			return false;
-		
-
 	}
+
 	public void DisplayHotel() {
 		System.out.println("\nHotels Present in Hotel Reservation System:");
 		for(int i=0;i<hotels.size();i++) {
 			System.out.println(hotels.get(i));
 		}
 	}
+
 	public static void main(String[] args) {
 		HotelReservation hotelObj = new HotelReservation();
 		hotelObj.addHotel();
 		hotelObj.DisplayHotel();
+		int ans, ch;
+		do {
+			System.out.println("Enter the choice : ");
+			System.out.println("1.Add ");
+			System.out.println("2.Display");
+			System.out.println("3.Exit");
+			ch = s.nextInt();
+			switch(ch) {
+			case 1: 
+				hotelObj.addHotel();
+				break;
+			case 2:
+				hotelObj.DisplayHotel();
+				break;
+			case 3:
+				break;
+
+			}System.out.println("Do you want to continue? if yes press '1' ");
+			ans = s.nextInt();
+		}while(ans == 1);
+		s.close();
 	}
 }
